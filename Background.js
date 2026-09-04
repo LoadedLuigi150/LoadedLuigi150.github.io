f95d0990431fae84f193d2122a6f921d95c46923
LoadedLuigi150 <calvin.legrand495@students.cmcss.net>
2026-09-04 15:23:23 -0500

    Update Background.js

--- Background.js
+++ Background.js
@@ -2,7 +2,6 @@ const background = document.createElement('img')
 background.style.width = '490px';
 background.style.height = '680px';
 background.style.position = 'absolute';
-background.style.left = '50%';
 background.src = 'Background.png';
 
 background.style.transform = 'translateX('+ window.innerWidth - (parseInt(background.style.width)/2) +'px)'
