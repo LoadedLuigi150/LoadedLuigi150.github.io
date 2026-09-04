6771a25630b94abb3c83b66818b1a0d91495c70c
LoadedLuigi150 <calvin.legrand495@students.cmcss.net>
2026-09-04 15:25:34 -0500

    Update Background.js

--- Background.js
+++ Background.js
@@ -4,6 +4,6 @@ background.style.height = '680px';
 background.style.position = 'absolute';
 background.src = 'Background.png';
 
-background.style.transform = 'translateX('+ window.innerWidth - (parseInt(background.style.width)/2) +'px)'
+background.style.transform = 'translateX('+ window.innerWidth +'px)'
 
 document.body.appendChild(background);
