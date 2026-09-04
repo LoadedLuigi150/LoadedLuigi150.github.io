ef880f513bd89a6cd81421fddc5c7dd3d7d27267
LoadedLuigi150 <calvin.legrand495@students.cmcss.net>
2026-09-04 15:15:25 -0500

    Create Background.js

---
+++ Background.js
@@ -0,0 +1,7 @@
+const background = document.createElement('img')
+background.style.width = '490px';
+background.style.height = '680px';
+background.style.position = 'absolute';
+background.style.left = '50%';
+
+background.style.transform = 'translateX('+ window.innerWidth - (parseInt(background.style.width)/2) +'px)'
